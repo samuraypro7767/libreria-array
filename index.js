5f2a762a6de9c1390ef733fc97b3088627d8c180
samuraypro7767 <sebaspro7767@gmail.com>
2024-05-23 11:38:10 -0500

    setup inicial proyecto practica array

---
+++ index.js
@@ -0,0 +1,625 @@
+
+
+
+let libros = [
+
+{
+        titulo: "El Señor de los Anillos",
+        autor: "J.R.R. Tolkien",
+        genero: "Fantasía Épica",
+        idioma: "Inglés",
+        precio: 25.99,
+        formato: "Físico",
+        isbn: "978-0-007-45355-6",
+        descripcion: "Trilogía de libros que narra la historia de la Guerra del Anillo en la Tierra Media.",
+        estado: "Nuevo",
+        ubicacion: "Biblioteca de la Casa",
+        fecha_publicacion: "29 de julio de 1954",
+        editorial: "Allen & Unwin",
+        paginas: 1184,
+        dimensiones: "20.3 x 13.5 x 4.5 cm",
+        peso: "680 gramos"
+}, 
+{
+        titulo: "El Hobbit",
+        autor: "J.R.R. Tolkien",
+        genero: "Fantasía Épica",
+        idioma: "Inglés",
+        precio: 25.99,
+        formato: "Físico",
+        isbn: "978-0-007-45355-6",
+        descripcion: "Trilogía de libros que narra la historia de la Guerra del Anillo en la Tierra Media.",
+        estado: "Nuevo",
+        ubicacion: "Biblioteca de la Casa",
+        fecha_publicacion: "29 de julio de 1954",
+        editorial: "Allen & Unwin",
+        paginas: 1184,
+        dimensiones: "20.3 x 13.5 x 4.5 cm",
+        peso: "680 gramos",
+},
+{
+    titulo: "Cronicas de Narnia",
+    autor: "C.S. Lewis",
+    genero: "Fantasía",
+    idioma: "Inglés",
+    precio: 20.99,
+    formato: "Físico",
+    isbn: "978-0-006-72233-2",
+    descripcion: "Serie de siete libros que narra la historia de Narnia, un mundo mágico creado por el león Aslan.",
+    estado: "Nuevo",
+    ubicacion: "Biblioteca de la Casa",
+    fecha_publicacion: "16 de octubre de 1950",
+    editorial: "Allen & Unwin",
+    paginas: 768,
+    dimensiones: "20.3 x 13.5 x 4.5 cm",
+    peso: "600 gramos",
+},
+{
+  titulo: "La sombra del viento",
+  autor: "Carlos Ruiz Zafón",
+  genero: "Novela histórica",
+  idioma: "Español",
+  precio: 19.99,
+  formato: "Físico",
+  isbn: "978-84-670-2641-5",
+  descripcion: "Una novela que narra la historia de un joven escritor que se ve envuelto en una trama de misterio y suspense en la Barcelona de los años 40.",
+  estado: "Nuevo",
+  ubicacion: "Estante 2, Biblioteca de la Ciudad",
+  fecha_publicacion: "15 de noviembre de 2009",
+  editorial: "Allen & Unwin",
+  paginas: 480,
+  dimensiones: "22.5 x 14.5 x 3.5 cm",
+  peso: "500 gramos",
+},
+{
+  titulo: "1984",
+  autor: "George Orwell",
+  genero: "Dystopía",
+  idioma: "Inglés",
+  precio: 15.99,
+  formato: "Físico",
+  isbn: "978-0-451-eda60-9",
+  descripcion: "Una novela clásica que describe un futuro distópico en el que el gobierno totalitario controla todos los aspectos de la vida.",
+  estado: "Usado",
+  ubicacion: "Estante 1, Biblioteca de la Ciudad",
+  fecha_publicacion: "8 de junio de 1949",
+  editorial: "Scribner",
+  paginas: 328,
+  dimensiones: "20.5 x 13.5 x 2.5 cm",
+  peso: "350 gramos",
+},
+{
+  titulo: "El arte de la guerra",
+  autor: "Sun Tzu",
+  genero: "Ensayo",
+  idioma: "Español",
+  precio: 12.99,
+  formato: "Digital",
+  isbn: "978-84-670-2641-5",
+  descripcion: "Un clásico de la estrategia y la táctica militar, que ofrece consejos y principios para ganar batallas y conflictos.",
+  estado: "Nuevo",
+  ubicacion: "Biblioteca digital",
+  fecha_publicacion: "Siglo VI a.C.",
+  editorial: "Scribner",
+  paginas: 128,
+  dimensiones: "N/A",
+  peso: "N/A",
+},
+{
+  titulo: "El gran Gatsby",
+  autor: "F. Scott Fitzgerald",
+  genero: "Novela modernista",
+  idioma: "Inglés",
+  precio: 15.99,
+  formato: "Físico",
+  isbn: "978-0-451-eda60-9",
+  descripcion: "Una novela clásica que describe la vida de Jay Gatsby, un misterioso millonario que vive en la costa este de Estados Unidos en los años 20.",
+  estado: "Usado",
+  ubicacion: "Estante 1, Biblioteca de la Ciudad",
+  fecha_publicacion: "10 de abril de 1925",
+  editorial: "Scribner",
+  paginas: 218,
+  dimensiones: "20.5 x 13.5 x 2.5 cm",
+  peso: "350 gramos",
+},
+{
+  titulo: "El mundo de Sofía",
+  autor: "Jostein Gaarder",
+  genero: "Novela filosófica",
+  idioma: "Español",
+  precio: 12.99,
+  formato: "Digital",
+  isbn: "978-84-670-2641-5",
+  descripcion: "Una novela que narra la historia de Sofía, una joven que descubre el mundo de la filosofía a través de una serie de cartas que recibe de un misterioso filósofo.",
+  estado: "Nuevo",
+  ubicacion: "Biblioteca digital",
+  fecha_publicacion: "1991",
+  editorial: "Scribner",
+  paginas: 328,
+  dimensiones: "N/A",
+  peso: "N/A",
+},
+{
+  titulo: "El diario de Ana Frank",
+  autor: "Ana Frank",
+  genero: "Memorias",
+  idioma: "Español",
+  precio: 15.99,
+  formato: "Físico",
+  isbn: "978-0-451-eda60-9",
+  descripcion: "Un libro que narra la historia de Ana Frank, una joven judía que se escondió con su familia en un ático en Amsterdam durante la Segunda Guerra Mundial.",
+  estado: "Usado",
+  ubicacion: "Estante 1, Biblioteca de la Ciudad",
+  fecha_publicacion: "25 de junio de 1947",
+  editorial: "Scribner",
+  paginas: 218,
+  dimensiones: "20.5 x 13.5 x 2.5 cm",
+  peso: "350 gramos",
+},
+{
+  titulo: "El principito",
+  autor: "Antoine de Saint-Exupéry",
+  genero: "Novela filosófica",
+  idioma: "Español",
+  precio: 12.99,
+  formato: "Digital",
+  isbn: "978-84-670-2641-5",
+  descripcion: "Una novela que narra la historia de un piloto que se estrella en el desierto y conoce a un niño proveniente de un asteroide.",
+  estado: "Nuevo",
+  ubicacion: "Biblioteca digital",
+  fecha_publicacion: "6 de abril de 1943",
+  editorial: " Allen & Unwin",
+  paginas: 96,
+  dimensiones: "N/A",
+  peso: "N/A",
+},
+{
+  titulo: "La isla del tesoro",
+  autor: "Robert Louis Stevenson",
+  genero: "Aventura",
+  idioma: "Español",
+  precio: 12.99,
+  formato: "Físico",
+  isbn: "978-0-451-eda60-9",
+  descripcion: "Una novela clásica que narra la historia de un joven llamado Jim Hawkins que se embarca en un viaje para encontrar un tesoro en una isla misteriosa.",
+  estado: "Usado",
+  ubicacion: "Estante 2, Biblioteca de la Ciudad",
+  fecha_publicacion: "1883",
+  editorial: "Cassell and Company",
+  paginas: 240,
+  dimensiones: "20.5 x 13.5 x 2.5 cm",
+  peso: "350 gramos",
+},
+{
+  titulo: "El código de Da Vinci",
+  autor: "Dan Brown",
+  genero: "Thriller",
+  idioma: "Inglés",
+  precio: 15.99,
+  formato: "Digital",
+  isbn: "978-0-385-50420-5",
+  descripcion: "Una novela que sigue la historia de un profesor de simbología que se ve envuelto en un misterio relacionado con la Iglesia Católica y el Santo Grial.",
+  estado: "Nuevo",
+  ubicacion: "Biblioteca digital",
+  fecha_publicacion: "18 de marzo de 2003",
+  editorial: "Cassell and Company",
+  paginas: 454,
+  dimensiones: "N/A",
+  peso: "N/A",
+},
+{
+  titulo: "El hombre en busca de sentido",
+  autor: "Viktor Frankl",
+  genero: "Ensayo",
+  idioma: "Español",
+  precio: 12.99,
+  formato: "Físico",
+  isbn: "978-0-451-eda60-9",
+  descripcion: "Un libro que narra la experiencia del autor en los campos de concentración nazis y su teoría sobre la logoterapia.",
+  estado: "Usado",
+  ubicacion: "Estante 3, Biblioteca de la Ciudad",
+  fecha_publicacion: "1946",
+  editorial: "Cassell and Company",
+  paginas: 160,
+  dimensiones: "20.5 x 13.5 x 2.5 cm",
+  peso: "350 gramos",
+},
+{
+  titulo: "El alquimista",
+  autor: "Paulo Coelho",
+  genero: "Ficción espiritual",
+  idioma: "Español",
+  precio: 15.99,
+  formato: "Digital",
+  isbn: "978-84-670-2641-5",
+  descripcion: "Una novela que narra la historia de un joven pastor andaluz que emprende un viaje para encontrar un tesoro en el desierto.",
+  estado: "Nuevo",
+  ubicacion: "Biblioteca digital",
+  fecha_publicacion: "1988",
+  editorial: "Cassell and Company",
+  paginas: 192,
+  dimensiones: "N/A",
+  peso: "N/A",
+},
+{
+  titulo: "El gran Gatsby",
+  autor: "F. Scott Fitzgerald",
+  genero: "Novela modernista",
+  idioma: "Inglés",
+  precio: 15.99,
+  formato: "Físico",
+  isbn: "978-0-451-eda60-9",
+  descripcion: "Una novela clásica que describe la vida de Jay Gatsby, un misterioso millonario que vive en la costa este de Estados Unidos en los años 20.",
+  estado: "Usado",
+  ubicacion: "Estante 1, Biblioteca de la Ciudad",
+  fecha_publicacion: "10 de abril de 1925",
+  editorial: "Cassell and Company",
+  paginas: 218,
+  dimensiones: "20.5 x 13.5 x 2.5 cm",
+  peso: "350 gramos",
+},
+{
+  titulo: "El mundo del amor",
+  autor: "carlos Gaarder",
+  genero: "Novela filosófica",
+  idioma: "Español",
+  precio: 12.99,
+  formato: "Digital",
+  isbn: "978-84-670-2641-5",
+  descripcion: "Una novela que narra la historia de Sofía, una joven que descubre el mundo de la filosofía a través de una serie de cartas que recibe de un misterioso filósofo.",
+  estado: "Nuevo",
+  ubicacion: "Biblioteca digital",
+  fecha_publicacion: "1991",
+  editorial: "Contact",
+  paginas: 328,
+  dimensiones: "N/A",
+  peso: "N/A",
+},
+{
+  titulo: "El diario de luis Frank",
+  autor: "Luis Frank",
+  genero: "Memorias",
+  idioma: "Español",
+  precio: 15.99,
+  formato: "Físico",
+  isbn: "978-0-451-eda60-9",
+  descripcion: "Un libro que narra la historia de luis Frank, una joven judía que se escondió con su familia en un ático en Amsterdam durante la Segunda Guerra Mundial.",
+  estado: "Usado",
+  ubicacion: "Estante 1, Biblioteca de la Ciudad",
+  fecha_publicacion: "25 de junio de 1947",
+  editorial: "Contact",
+  paginas: 218,
+  dimensiones: "20.5 x 13.5 x 2.5 cm",
+  peso: "350 gramos",
+},
+{
+  titulo: "La isla de oro",
+  autor: "carl davison",
+  genero: "Aventura",
+  idioma: "Español",
+  precio: 12.99,
+  formato: "Físico",
+  isbn: "978-0-451-eda60-9",
+  descripcion: "Una novela clásica que narra la historia de un joven llamado deivid Hawkins que se embarca en un viaje para encontrar un tesoro en una isla misteriosa.",
+  estado: "Usado",
+  ubicacion: "Estante 2, Biblioteca de la Ciudad",
+  fecha_publicacion: "1883",
+  editorial: "Contact",
+  paginas: 240,
+  dimensiones: "20.5 x 13.5 x 2.5 cm",
+  peso: "350 gramos",
+},
+{
+  titulo: "El código de fibonanci",
+  autor: "Dilan Brown",
+  genero: "Thriller",
+  idioma: "Inglés",
+  precio: 15.99,
+  formato: "Digital",
+  isbn: "978-0-385-50420-5",
+  descripcion: "Una novela que sigue la historia de un profesor de simbología que se ve envuelto en un misterio relacionado con la Iglesia Católica y el Santo Grial.",
+  estado: "Nuevo",
+  ubicacion: "Biblioteca digital",
+  fecha_publicacion: "18 de marzo de 2003",
+  editorial: "Contact",
+  paginas: 454,
+  dimensiones: "N/A",
+  peso: "N/A",
+},
+{
+  titulo: "El hombre en busca de sentido",
+  autor: "Viktor Frankl",
+  genero: "Ensayo",
+  idioma: "Español",
+  precio: 12.99,
+  formato: "Físico",
+  isbn: "978-0-451-eda60-9",
+  descripcion: "Un libro que narra la experiencia del autor en los campos de concentración nazis y su teoría sobre la logoterapia.",
+  estado: "Usado",
+  ubicacion: "Estante 3, Biblioteca de la Ciudad",
+  fecha_publicacion: "1946",
+  editorial: "Contact",
+  paginas: 160,
+  dimensiones: "20.5 x 13.5 x 2.5 cm",
+  peso: "350 gramos",
+}
+
+];
+
+function agregarLibro() {
+  let libro = {
+    titulo : prompt('titulo'), 
+    autor : prompt('autor'),
+    genero : prompt('genero'),
+    idioma : prompt('idioma'),
+    precio : prompt('precio'),
+    formato : prompt('formato'),
+    isbn : prompt('isbn'),
+    drescripcion : prompt('drescripcion'), 
+    estado : prompt('estado'),
+    ubicacion : prompt('ubicacion'),
+    fecha : prompt('fecha'),
+    editorial : prompt('editorial'),
+    paginas : prompt('paginas'), 
+    dimensiones : prompt('dimensiones'),
+    peso : prompt('peso')
+
+  }
+
+
+  console.log("antes de añadir");
+  console.table(libros);
+  console.log("libro añadido");
+  libros.push(libro);
+  console.table(libro);
+  console.log("Actualmente");
+  console.table(libros);
+}
+
+function quitarLibro() {
+  let libroDeleted = libros.pop();
+  console.log("libro eliminado");
+  console.table(libroDeleted);
+  console.log("pila de libros Actualmente :");
+  console.table(libros);
+}
+
+function mostrarLibros() {
+  console.log("libros actuales :");
+  console.table(libros);
+}
+
+function Menu() {
+  menu=("Menú de opciones:\n\n"),
+  menu+=("1. Agregar libro\n"),
+  menu+=("2. Quitar libro\n"),
+  menu+=("3. Mostrar libros\n"),
+  menu+=("4. ver titulo\n"),
+  menu+=("5. ver paginas\n"),
+  menu+=("6. ver idioma\n"),
+  menu+=("7. ver fecha\n"),
+  menu+=("8. ver estado\n"),
+  menu+=("9. ver formato\n"),
+  menu+=("10. ver peso\n"),
+  menu+=("11. ver dimesiones\n"),
+  menu+=("12. ver ubicacion\n"),
+  menu+=("13. ver ISBN\n"),
+  menu+=("14. ver descripcion\n"),
+  menu+=("15. ver descuento total\n"),
+  menu+=("16. Salir\n\n"),
+  menu+=("Ingrese la opcion :");
+  return parseInt(prompt(menu));
+  
+}
+
+function ejecutarMenu() {
+ 
+  let opcion = Menu();
+ switch (opcion) {
+   case 1:
+     agregarLibro();
+     break;
+   case 2:
+     quitarLibro();
+     break;
+   case 3:
+     mostrarLibros();
+     break;
+   case 4:
+    console.table(TITULO);
+     break;
+   case 5:
+    console.table(PAGINAS);
+     break;
+   case 6:
+    console.table(IDIOMA);
+     break;
+   case 7:
+    console.table(FECHA);
+     break;
+   case 8:
+    console.table(ESTADO);
+     break;
+   case 9:
+    console.table(FORMATO);
+     break;
+   case 10:
+    console.table(DIMENSIONES);
+     break;
+   case 11:
+    console.table(PESO);
+     break;
+   case 12:
+    console.table(UBICACION);
+     break;
+   case 13:
+    console.table(ISBN);
+     break;
+   case 14:
+    console.table(DESCRIPCION);
+     break;
+   case 15:
+    Descuento();
+     break;
+   case 16:
+     console.log("Hasta luego!");
+     opcion = false;
+     break;
+   default:
+     console.log("Opción inválida, intente de nuevo.");
+ }
+ 
+ 
+ }
+ 
+ function Menu2() {
+  ejecutarMenu();
+  return confirm('Desea continuar en el menu');
+}
+
+function EjecutarMenu2() {
+  if (Menu2()) {
+    ejecutarMenu();
+    EjecutarMenu2();
+  } else {
+    alert('Salió del menú');
+  }
+}
+
+
+const TITULO = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    precio: libros.precio
+}
+});
+
+const PAGINAS = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    Paginas: libros.paginas, 
+   
+}
+});
+
+const IDIOMA = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    idioma: libros.idioma
+}
+});
+
+const FECHA = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    fecha_publicacion: libros.fecha_publicacion
+}
+});
+
+const ESTADO = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    estado: libros.estado
+}
+});
+
+const FORMATO = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    formato: libros.formato
+}
+});
+
+const PESO = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    peso: libros.peso
+}
+});
+
+const DIMENSIONES = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    dimensiones: libros.dimensiones
+}
+});
+
+const UBICACION = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    fecha: libros.ubicacion
+}
+});
+
+const ISBN = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    isbn: libros.isbn
+}
+});
+
+const DESCRIPCION = libros.map((libros) => {
+
+  return{
+    titulo: libros.titulo , 
+    author: libros.autor,
+    editorial: libros.editorial,
+    descripcion: libros.descripcion
+}
+});
+
+function Descuento() {
+  const DESCUENTO = libros.map(function(libro) {
+      return {
+          ...libro,
+          descuento: 20,
+          precio_final: libro.precio - (libro.precio * 0.2)
+      };
+  });
+
+  const DESCUENTO_PRECIO = DESCUENTO.map(function(libro) {
+      return {
+          titulo: libro.titulo, 
+          autor: libro.autor, 
+          editorial: libro.editorial, 
+          precio: libro.precio,
+          descuento: libro.descuento,
+          precio_final: libro.precio_final
+      };
+  });
+
+  return console.table(DESCUENTO_PRECIO);
+}
+// console.table(DESCUENTO);
+
+EjecutarMenu2();
